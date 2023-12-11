119e670dcaeb6a21d2927db6bc9ea501b47006f6
Frallen <bladone439@gmail.com>
2023-12-11 12:46:31 +0300

    init

---
+++ .output/server/chunks/app/client.manifest.mjs
@@ -0,0 +1,167 @@
+const client_manifest = {
+  "_index.b8fe2cb5.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "index.b8fe2cb5.js"
+  },
+  "_vue.f36acd1f.92eea515.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "vue.f36acd1f.92eea515.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.8.2_@types+node@20.10.4_vite@5.0.6/node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "node_modules/.pnpm/@nuxt+ui-templates@1.3.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "error-404.871584dc.css",
+    "src": "node_modules/.pnpm/@nuxt+ui-templates@1.3.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.css"
+  },
+  "node_modules/.pnpm/@nuxt+ui-templates@1.3.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [
+      "error-404.871584dc.css"
+    ],
+    "file": "error-404.505598b6.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.8.2_@types+node@20.10.4_vite@5.0.6/node_modules/nuxt/dist/app/entry.js",
+      "_vue.f36acd1f.92eea515.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/.pnpm/@nuxt+ui-templates@1.3.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue"
+  },
+  "error-404.871584dc.css": {
+    "file": "error-404.871584dc.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/.pnpm/@nuxt+ui-templates@1.3.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "error-500.5504fd74.css",
+    "src": "node_modules/.pnpm/@nuxt+ui-templates@1.3.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.css"
+  },
+  "node_modules/.pnpm/@nuxt+ui-templates@1.3.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [
+      "error-500.5504fd74.css"
+    ],
+    "file": "error-500.1b4acfd9.js",
+    "imports": [
+      "_vue.f36acd1f.92eea515.js",
+      "node_modules/.pnpm/nuxt@3.8.2_@types+node@20.10.4_vite@5.0.6/node_modules/nuxt/dist/app/entry.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/.pnpm/@nuxt+ui-templates@1.3.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+  },
+  "error-500.5504fd74.css": {
+    "file": "error-500.5504fd74.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/.pnpm/nuxt-icon@0.6.7_nuxt@3.8.2_vite@5.0.6_vue@3.3.10/node_modules/nuxt-icon/dist/runtime/Icon.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "Icon.f484574a.css",
+    "src": "node_modules/.pnpm/nuxt-icon@0.6.7_nuxt@3.8.2_vite@5.0.6_vue@3.3.10/node_modules/nuxt-icon/dist/runtime/Icon.css"
+  },
+  "node_modules/.pnpm/nuxt-icon@0.6.7_nuxt@3.8.2_vite@5.0.6_vue@3.3.10/node_modules/nuxt-icon/dist/runtime/Icon.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [
+      "Icon.f484574a.css"
+    ],
+    "file": "Icon.1ecb9514.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.8.2_@types+node@20.10.4_vite@5.0.6/node_modules/nuxt/dist/app/entry.js",
+      "_index.b8fe2cb5.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/.pnpm/nuxt-icon@0.6.7_nuxt@3.8.2_vite@5.0.6_vue@3.3.10/node_modules/nuxt-icon/dist/runtime/Icon.vue"
+  },
+  "Icon.f484574a.css": {
+    "file": "Icon.f484574a.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/.pnpm/nuxt-icon@0.6.7_nuxt@3.8.2_vite@5.0.6_vue@3.3.10/node_modules/nuxt-icon/dist/runtime/IconCSS.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "IconCSS.6f939f74.css",
+    "src": "node_modules/.pnpm/nuxt-icon@0.6.7_nuxt@3.8.2_vite@5.0.6_vue@3.3.10/node_modules/nuxt-icon/dist/runtime/IconCSS.css"
+  },
+  "node_modules/.pnpm/nuxt-icon@0.6.7_nuxt@3.8.2_vite@5.0.6_vue@3.3.10/node_modules/nuxt-icon/dist/runtime/IconCSS.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [
+      "IconCSS.6f939f74.css"
+    ],
+    "file": "IconCSS.9479f8ab.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.8.2_@types+node@20.10.4_vite@5.0.6/node_modules/nuxt/dist/app/entry.js",
+      "_index.b8fe2cb5.js"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/.pnpm/nuxt-icon@0.6.7_nuxt@3.8.2_vite@5.0.6_vue@3.3.10/node_modules/nuxt-icon/dist/runtime/IconCSS.vue"
+  },
+  "IconCSS.6f939f74.css": {
+    "file": "IconCSS.6f939f74.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/.pnpm/nuxt@3.8.2_@types+node@20.10.4_vite@5.0.6/node_modules/nuxt/dist/app/entry.css": {
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true,
+    "file": "entry.db3c5f97.css",
+    "src": "node_modules/.pnpm/nuxt@3.8.2_@types+node@20.10.4_vite@5.0.6/node_modules/nuxt/dist/app/entry.css"
+  },
+  "node_modules/.pnpm/nuxt@3.8.2_@types+node@20.10.4_vite@5.0.6/node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "css": [
+      "entry.db3c5f97.css"
+    ],
+    "dynamicImports": [
+      "node_modules/.pnpm/@nuxt+ui-templates@1.3.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+      "node_modules/.pnpm/@nuxt+ui-templates@1.3.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+    ],
+    "file": "entry.c2d83d48.js",
+    "isEntry": true,
+    "src": "node_modules/.pnpm/nuxt@3.8.2_@types+node@20.10.4_vite@5.0.6/node_modules/nuxt/dist/app/entry.js"
+  },
+  "entry.db3c5f97.css": {
+    "file": "entry.db3c5f97.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
